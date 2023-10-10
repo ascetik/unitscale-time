2b99dda69da878ac36611e30c377b79b93469ce2
ascetik <ascetik@free.fr>
2023-10-10 18:07:57 +0200

    DetailedTimeVaue handles decomposed adjustment

--- src/Extensions/DetailedTimeValue.php
+++ src/Extensions/DetailedTimeValue.php
@@ -2,26 +2,37 @@
 
 namespace Ascetik\UnitscaleTime\Extensions;
 
-use Ascetik\UnitscaleCore\Extensions\AdjustedValue;
-use Ascetik\UnitscaleCore\Traits\UseHighestValue;
-use Ascetik\UnitscaleTime\DTO\TimeScaleReference;
+use Ascetik\UnitscaleCore\Parsers\ScaleCommandParser;
+use Ascetik\UnitscaleCore\Types\Scale;
+use Ascetik\UnitscaleTime\Extensions\AdjustedTimeValue;
 use Ascetik\UnitscaleTime\Values\TimeScaleValue;
 
-class DetailedTimeValue extends AdjustedValue
+/**
+ * Decompose given Time value by multiple chained scales
+ *
+ * @mixin AdjustedTimeValue
+ * @version 1.0.0
+ */
+class DetailedTimeValue extends AdjustedTimeValue
 {
-    use UseHighestValue;
-
     /**
-     * TODO : Implementation
+     * Next value to display
      *
-     * Cet objet servira à détailler un TimeScaleValue
-     * exactement comme le fait l'actuel AdjustedTimeValue.
+     * @var self|null
      */
     private ?self $next = null;
+    
+    protected ?Scale $lowest = null;
 
-    public function __construct(private TimeScaleReference $reference)
+    public function __call($name, $arguments): static
     {
-        $this->setReference($reference);
+        $parser = new ScaleCommandParser('as', 'until');
+        $command = $parser->parse($name);
+        $reference = match ($command->prefix) {
+            'as' => $this->reference->limitTo($command->name),
+            'until' => $this->until($command->name)
+        };
+        return new static($reference);
     }
 
     public function __toString(): string
@@ -44,9 +55,18 @@ class DetailedTimeValue extends AdjustedValue
         return implode(' ', $output);
     }
 
+    private function until(string $scale)
+    {
+        $this->lowest = TimeScaleValue::createScale($scale);
+    }
+
     private function setNextWith(int|float $value)
     {
         $leftValue = new TimeScaleValue($value);
-        $this->next = new static($this->reference->useValue($leftValue));
+        $ref = $this->reference->useValue($leftValue);
+        // $highest = $ref->withHighestValue();
+        // echo $highest->value->getUnit() . PHP_EOL;
+        // echo ($this->lowest?->unit() ?? 'pas de lowest sur ce coup là !') . PHP_EOL;
+        $this->next = new static($ref);
     }
 }
